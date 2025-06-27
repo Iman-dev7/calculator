fde545836340b431fe88bdbe258e118f02f5a0dd
Iman-dev7 <imaninspires1@gmail.com>
2025-06-27 12:11:02 +0300

    added the function display

--- script.js
+++ script.js
@@ -19,6 +19,30 @@ function divide(a,b){
     return a/b;
 }
 
-let num1 =''
-let operator = ''
-let num2 = ''
+let num1 ='';
+let operator = '';
+let num2 = '';
+
+function operate(operator, a, b){
+    switch(operator){
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply([a, b]);
+        case '/':
+            return divide(a, b);
+        default:
+            return 0;
+    }
+}
+
+
+let currentValue = ''
+
+function display(digit){
+    currentValue += digit;
+    document.getElementById('calculator-input').value = currentValue;
+
+}
