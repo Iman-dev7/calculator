36eca21f7de8a459f5119ace0af8734bb8059c7f
Iman-dev7 <imaninspires1@gmail.com>
2025-06-27 12:06:40 +0300

    added the logic for add

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(arr){
+    if(arr.length === 0)  return 0;
+    return arr.reduce((total,currentValue)=> total * currentValue, 1);
+
+}
+
+function divide(a,b){
+    if(b===0){
+        throw new Error("undefined")
+    }
+    return a/b;
+}
